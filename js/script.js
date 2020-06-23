a4d2921851ba2e6cda443b99db99347ef7c92779
Abhishek Rathore <abhishekrathore325@gmail.com>
2020-06-23 15:43:27 +0530

    practicing bootstrap

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
+ $("#buttonlogin").click(function() {
+    $("#loginModal").modal('show');
+});
+
+
+$("#buttonreserve").click(function() {
+    $("#reserveform").modal('show');
+});
